d62ec65e154f74b7d698ad55f6ecb8287fa28fcd
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-06-05 16:01:48 +0200

    Setup supabase

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+
+import { supabase } from '@/lib/supabaseClient'
+
+console.log(supabase)
 </script>
 
 <template>

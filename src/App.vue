6532f3098ff4af40e580d17984443c569303e6e4
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-07-15 13:46:49 +0200

    Add shad-cn components and tailwind.css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
+import { Input } from '@/components/ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
 import { supabase } from '@/lib/supabaseClient'
 
 console.log(supabase)
 </script>
 
 <template>
-  <RouterView />
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <Input class="w-full pl-8 bg-background" placeholder="Search..." />
+    </form>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   plugins: [VueRouter(), vue()],
   resolve: {
     alias: {

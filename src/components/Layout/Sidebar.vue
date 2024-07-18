1af5ff1c351efe0cc33e58e8c886ca80d8c51d36
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-07-18 09:08:39 +0200

    Re-usable link component

---
+++ src/components/Layout/Sidebar.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import SidebarLink from '@/components/Layout/SidebarLink.vue'
+
+const links = [
+  { title: 'Dashboard', to: '/dashboard', icon: 'lucide:home' },
+  { title: 'Projects', to: '/projects', icon: 'lucide:folder' },
+  { title: 'My Tasks', to: '/tasks', icon: 'lucide:check-circle' }
+]
+
+const accountLinks = [
+  { title: 'Profile', to: '/profile', icon: 'lucide:user' },
+  { title: 'Settings', to: '/settings', icon: 'lucide:settings' },
+  { title: 'Sign out', to: '/signout', icon: 'lucide:log-out' }
+]
+</script>
+
+<template>
+  <aside
+    class="flex flex-col h-screen gap-2 border-r fixed bg-muted/40 lg:w-52 w-16 transition-[width]"
+  >
+    <div class="flex h-16 items-center border-b px-2 lg:px-4 shrink-0 gap-1 justify-between">
+      <button variant="outline" size="icon" class="w-8 h-8">
+        <iconify-icon icon="lucide:menu"></iconify-icon>
+      </button>
+
+      <button variant="outline" size="icon" class="w-8 h-8">
+        <iconify-icon icon="lucide:plus"></iconify-icon>
+      </button>
+    </div>
+
+    <nav class="flex flex-col gap-2 justify-between h-full relative">
+      <div>
+        <SidebarLink :links="links" />
+      </div>
+
+      <div class="border-y text-center bg-background py-3">
+        <SidebarLink :links="accountLinks" />
+      </div>
+    </nav>
+  </aside>
+</template>

4462cc64845be09e356f01db5c48828fb5917f61
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-09-24 16:35:07 +0200

    Add error page

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 import { usePageStore } from '@/stores/page'
+import ErrorPage from '@/components/AppError/AppErrorPage.vue'
 
 usePageStore().pageData.title = 'Not found'
-
-const route = useRoute()
 </script>
 
 <template>
-  <div>
-    <h1>Not found</h1>
-
-    <p>Sorry, {{ route.fullPath }} does not exist.</p>
-
-    <RouterLink to="/">Home</RouterLink>
-  </div>
+  <ErrorPage />
 </template>

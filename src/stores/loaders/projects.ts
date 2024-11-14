70506bc4881c7223dfa726f00b120a9be313e104
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-11-14 15:38:02 +0100

    Cache with useMemoize utiliy function

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,12 +1,13 @@
 import type { Projects } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 import { ProjectsQuery } from '@/utils/supaQueries'
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>([])
-  const getProjects = async () => {
-    if (projects.value?.length) return
+  const loadProjects = useMemoize(async (key: string) => await ProjectsQuery)
 
-    const { data, error, status } = await ProjectsQuery
+  const getProjects = async () => {
+    const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
 

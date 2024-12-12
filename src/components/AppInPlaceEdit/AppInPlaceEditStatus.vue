d91e8c630a9fcf69c171f91c32c7955d672e409b
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-12-12 16:27:10 +0100

    Update project status in database

--- src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 const value = defineModel<'in-progress' | 'completed'>()
+
+const emit = defineEmits(['commit'])
+
+const toggleValue = () => {
+  value.value = value.value === 'completed' ? 'in-progress' : 'completed'
+  emit('commit')
+}
 </script>
 
 <template>
-  <div class="text-2xl cursor-pointer">
+  <div class="text-2xl cursor-pointer" @click="toggleValue">
     <iconify-icon v-if="value === 'completed'" icon="lucide:circle-check" class="text-green-500" />
     <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" />
   </div>

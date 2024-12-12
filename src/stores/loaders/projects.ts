68224808cb906cd34c51eb8f58f1b3e8b3eba8e0
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-12-12 14:59:58 +0100

    Fix title update
    by resetting the ref variable before setting the value from memoize.

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -34,6 +34,8 @@ export const useProjectsStore = defineStore('projects-store', () => {
   }
 
   const getProjects = async () => {
+    projects.value = null
+
     const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
@@ -50,6 +52,8 @@ export const useProjectsStore = defineStore('projects-store', () => {
   }
 
   const getProject = async (slug: string) => {
+    project.value = null
+
     const { data, error, status } = await loadProject(slug)
 
     if (error) useErrorStore().setError({ error: error, customCode: status })

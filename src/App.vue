5f9246fdbf7c79f118a86b20fa792e1fdf326ddb
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2025-01-23 15:00:21 +0100

    Transform guest layout switch

--- src/App.vue
+++ src/App.vue
@@ -36,24 +36,26 @@ const GuestLayout = defineAsyncComponent(() => import('./components/Layout/main/
 </script>
 
 <template>
-  <Component :is="user ? AuthLayout : GuestLayout">
-    <AppErrorPage v-if="activeError" />
+  <Transition name="fade" mode="out-in">
+    <Component :is="user ? AuthLayout : GuestLayout" :key="user?.id">
+      <AppErrorPage v-if="activeError" />
 
-    <RouterView v-slot="{ Component, route }">
-      <Transition name="fade" mode="out-in">
-        <div class="w-full" :key="route.path">
-          <Suspense v-if="Component" :timeout="0.1">
-            <component :is="Component">Hi</component>
-            <template #fallback>
-              <div
-                class="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center w-full h-screen bg-background bg-opacity-90 z-50"
-              >
-                <iconify-icon icon="lucide:loader-circle" class="text-6xl animate-spin" />
-              </div>
-            </template>
-          </Suspense>
-        </div>
-      </Transition>
-    </RouterView>
-  </Component>
+      <RouterView v-slot="{ Component, route }">
+        <Transition name="fade" mode="out-in">
+          <div class="w-full" :key="route.path">
+            <Suspense v-if="Component" :timeout="0.1">
+              <component :is="Component">Hi</component>
+              <template #fallback>
+                <div
+                  class="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center w-full h-screen bg-background bg-opacity-90 z-50"
+                >
+                  <iconify-icon icon="lucide:loader-circle" class="text-6xl animate-spin" />
+                </div>
+              </template>
+            </Suspense>
+          </div>
+        </Transition>
+      </RouterView>
+    </Component>
+  </Transition>
 </template>
